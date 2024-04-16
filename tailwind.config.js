266d96c363d7d13ce8c9688b10ec0c1143a2305e
Jedidiah Amaraegbu <imunacode@gmail.com>
2024-04-16 20:48:46 +0100

    feat(app): splash screen and logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "twitter-blue": "#1D9BF0",
+      },
+    },
   },
   plugins: [],
 };
