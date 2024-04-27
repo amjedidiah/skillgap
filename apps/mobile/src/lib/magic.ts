1b6888eb75a1f65f01faed54bc99626103132c68
Jedidiah Amaraegbu <imunacode@gmail.com>
2024-04-27 21:48:33 +0100

    fix(app): expo build fix

--- apps/mobile/src/lib/magic.ts
+++ apps/mobile/src/lib/magic.ts
@@ -1,15 +1,15 @@
 import { Magic as MagicSDK } from "@magic-sdk/react-native-expo";
-import { isDev } from "@skillgap/shared/constants";
 
 const magic = new MagicSDK(
   process.env.EXPO_PUBLIC_MAGIC_PUBLISHABLE_API_KEY as string,
   {
-    testMode: isDev,
-    network: {
-      rpcUrl:
-        "https://eth-mainnet.g.alchemy.com/v2/fYFybLQFR9Zr2GCRcgALmAktStFKr0i0",
-      chainId: 1,
-    },
+    // testMode: isDev,
+    // network: {
+    //   rpcUrl:
+    //     "https://eth-mainnet.g.alchemy.com/v2/fYFybLQFR9Zr2GCRcgALmAktStFKr0i0",
+    //   chainId: 1,
+    // },
+    network: "mainnet",
   }
 );
 
